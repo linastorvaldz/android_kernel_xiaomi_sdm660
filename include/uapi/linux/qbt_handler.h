5ef7f1abc1ba91217a6391f6cd507952f3eb80eb
Abir Ghosh <abirg@codeaurora.org>
2019-07-15 12:21:01 +0530

    qbt: Fix touch event filtering logic
    Fix touch event codes in filtering logic.
    
    Change-Id: I4de929b4c4c8591a8372ae1b195db85c57acf974
    Signed-off-by: Abir Ghosh <abirg@codeaurora.org>

--- include/uapi/linux/qbt_handler.h
+++ include/uapi/linux/qbt_handler.h
@@ -32,6 +32,19 @@ enum qbt_finger_events {
 };
 
 /*
+ * enum qbt_finger_events -
+ *      enumeration of qbt finger events
+ * @QBT_EVENT_FINGER_UP - finger up detected
+ * @QBT_EVENT_FINGER_DOWN - finger down detected
+ * @QBT_EVENT_FINGER_MOVE - finger move detected
+ */
+enum qbt_finger_events {
+	QBT_EVENT_FINGER_UP,
+	QBT_EVENT_FINGER_DOWN,
+	QBT_EVENT_FINGER_MOVE
+};
+
+/*
  * enum qbt_fw_event -
  *      enumeration of firmware events
  * @FW_EVENT_FINGER_DOWN - finger down detected
