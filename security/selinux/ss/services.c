1b43cdc3ae3f3cc20ee1ae21317ae1024276b52b
xiaoleGun <1592501605@qq.com>
2024-04-20 19:42:26 +0800

    [SQUASH] arch/include/security: Bring back audit
    Revert "arm64: configs: wayne: Disable audit"
    
    This reverts commit ddf919dcebff463bb739ddc462091d51a516b8a4.
    
    Revert "selinux: Remove audit dependency"
    
    This reverts commit cc4332e25e125872cc1f3e55288f8c854e55606d.

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3579,7 +3579,6 @@ out:
 	return match;
 }
 
-#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3602,7 +3601,6 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
-#endif
 
 #ifdef CONFIG_NETLABEL
 /**
