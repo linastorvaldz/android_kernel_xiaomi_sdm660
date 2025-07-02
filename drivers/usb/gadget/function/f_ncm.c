9f813277795215c189b0296499febbe7af4f5757
LKDenchin <mwlkgdq@outlook.com>
2025-07-02 12:15:14 +0800

    Revert "Merge branch 'android-4.19-stable' of https://android.googlesource.com/kernel/common into caf_12_13_merges_upstream"
    This reverts commit ec28b90e341463dc7a6fb363391400f7c31dbe52.

--- drivers/usb/gadget/function/f_ncm.c
+++ drivers/usb/gadget/function/f_ncm.c
@@ -1180,8 +1180,7 @@ static int ncm_unwrap_ntb(struct gether *port,
 			  struct sk_buff_head *list)
 {
 	struct f_ncm	*ncm = func_to_ncm(&port->func);
-	unsigned char	*ntb_ptr = skb->data;
-	__le16		*tmp;
+	__le16		*tmp = (void *) skb->data;
 	unsigned	index, index2;
 	int		ndp_index;
 	unsigned	dg_len, dg_len2;
@@ -1194,10 +1193,6 @@ static int ncm_unwrap_ntb(struct gether *port,
 	const struct ndp_parser_opts *opts = ncm->parser_opts;
 	unsigned	crc_len = ncm->is_crc ? sizeof(uint32_t) : 0;
 	int		dgram_counter;
-	int		to_process = skb->len;
-
-parse_ntb:
-	tmp = (__le16 *)ntb_ptr;
 
 	/* dwSignature */
 	if (get_unaligned_le32(tmp) != opts->nth_sign) {
@@ -1244,7 +1239,7 @@ parse_ntb:
 		 * walk through NDP
 		 * dwSignature
 		 */
-		tmp = (__le16 *)(ntb_ptr + ndp_index);
+		tmp = (void *)(skb->data + ndp_index);
 		if (get_unaligned_le32(tmp) != ncm->ndp_sign) {
 			INFO(port->func.config->cdev, "Wrong NDP SIGN\n");
 			goto err;
@@ -1301,11 +1296,11 @@ parse_ntb:
 			if (ncm->is_crc) {
 				uint32_t crc, crc2;
 
-				crc = get_unaligned_le32(ntb_ptr +
+				crc = get_unaligned_le32(skb->data +
 							 index + dg_len -
 							 crc_len);
 				crc2 = ~crc32_le(~0,
-						 ntb_ptr + index,
+						 skb->data + index,
 						 dg_len - crc_len);
 				if (crc != crc2) {
 					INFO(port->func.config->cdev,
@@ -1332,7 +1327,7 @@ parse_ntb:
 							 dg_len - crc_len);
 			if (skb2 == NULL)
 				goto err;
-			skb_put_data(skb2, ntb_ptr + index,
+			skb_put_data(skb2, skb->data + index,
 				     dg_len - crc_len);
 
 			skb_queue_tail(list, skb2);
@@ -1345,17 +1340,10 @@ parse_ntb:
 		} while (ndp_len > 2 * (opts->dgram_item_len * 2));
 	} while (ndp_index);
 
-	VDBG(port->func.config->cdev,
-	     "Parsed NTB with %d frames\n", dgram_counter);
-
-	to_process -= block_len;
-	if (to_process != 0) {
-		ntb_ptr = (unsigned char *)(ntb_ptr + block_len);
-		goto parse_ntb;
-	}
-
 	dev_consume_skb_any(skb);
 
+	VDBG(port->func.config->cdev,
+	     "Parsed NTB with %d frames\n", dgram_counter);
 	return 0;
 err:
 	skb_queue_purge(list);
