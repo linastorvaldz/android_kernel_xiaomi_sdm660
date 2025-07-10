590e454be55467428ace354f973743c47d328e2b
LKDenchin <mwlkgdq@outlook.com>
2025-07-10 14:12:24 +0800

    Revert "Merge 4.19.311 into android-4.19-stable"
    This reverts commit 8f00c617fa9213464cf8397549a00228d3c57caf.

--- drivers/md/dm-crypt.c
+++ drivers/md/dm-crypt.c
@@ -46,11 +46,11 @@
 struct convert_context {
 	struct completion restart;
 	struct bio *bio_in;
-	struct bvec_iter iter_in;
 	struct bio *bio_out;
+	struct bvec_iter iter_in;
 	struct bvec_iter iter_out;
-	atomic_t cc_pending;
 	u64 cc_sector;
+	atomic_t cc_pending;
 	union {
 		struct skcipher_request *req;
 		struct aead_request *req_aead;
